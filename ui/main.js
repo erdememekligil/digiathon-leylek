cf4b9c1c826b0347fb03068278f849387ac831b1
Erdem Emekligil <erdememekligil@gmail.com>
2022-11-12 17:34:06 +0300

    ui design

--- ui/main.js
+++ ui/main.js
@@ -1,39 +1,95 @@
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const {app, ipcMain, BrowserWindow } = require('electron')
 
-const createWindow = () => {
+var path = require('path')
+require('./dialog/dialog')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+let secondWindow
+
+function createWindow () {
     // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 720,
+    mainWindow = new BrowserWindow({titleBarStyle: 'hidden',
+        width: 1281,
+        height: 800,
+        minWidth: 1281,
+        minHeight: 800,
+        backgroundColor: '#312450',
+        show: false,
+        icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            nodeIntegration: true
         }
     })
 
     // and load the index.html of the app.
-    mainWindow.loadFile('index.html')
+    //.loadFile ?
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
 
     // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
+
+
+    // Show the mainwindow when it is loaded and ready to show
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+
+    secondWindow = new BrowserWindow({frame: false,
+        width: 800,
+        height: 600,
+        minWidth: 800,
+        minHeight: 600,
+        backgroundColor: '#312450',
+        show: false,
+        icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
+        parent: mainWindow,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    secondWindow.loadURL(`file://${__dirname}/windows/ipcwindow.html`)
 }
 
-// This method will be called when Electron has finished initialization and is ready to create browser windows.
+ipcMain.on('open-second-window', (event, arg)=> {
+    secondWindow.show()
+})
+
+ipcMain.on('close-second-window', (event, arg)=> {
+    secondWindow.hide()
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-    createWindow()
+app.on('ready', createWindow)
 
-    app.on('activate', () => {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common for applications and their menu bar to
-// stay active until the user quits explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
