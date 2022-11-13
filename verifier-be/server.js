da2738bd4ea85b833afd756c8d025f328ea1bbd6
Erdem Emekligil <erdememekligil@gmail.com>
2022-11-13 09:36:11 +0300

    verifier be init

---
+++ verifier-be/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const uuid = require('uuid');
+const PDFDocument = require('pdfkit');
+const keys = require("./keys.js");
+const encdec = require("./encdec");
+
+
+const status_db = {};
+const documents = ["Askerlik Durum Belgesi", "SGK Hizmet Dökümü", "Sabıka Kaydı", "İkametgah"]
+
+app.get('/status', function (req, res) {
+    console.log("status check");
+    var values = Object.keys(status_db).map(function(key){
+        return status_db[key];
+    });
+    res.send(values);
+})
+
+app.post('/requestDocuments', function (req, res) {
+    const guid = uuid.v4();
+    console.log("requestDocuments request " + guid);
+
+    // add document request to blockchain
+    //TODO
+
+    status_db[guid] = {"guid":guid, "status": "requested", "documents": documents}
+})
+
+app.post('/postDocuments', function (req, res) {
+    const guid = 123;  // get guid from request TODO
+    console.log("postDocuments request " + guid);
+
+    // verify and update status_db
+    // TODO
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
