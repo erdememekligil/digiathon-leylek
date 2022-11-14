f3c2ea467ff21872fc2391495c411c6590dd6759
Murat Güç <gucmurat>
2022-11-14 17:15:53 +0300

    tasks about reading/writing hash on bc are added.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
 //require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config()
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const cfg = require("./scripts/config.js");
 
-const DIGI_PRIVATE_KEY = process.env.DIGI0_WALLET_PRIVATE_KEY;
-const PRIVATE_KEY = process.env.AVAX_WALLET_PRIVATE_KEY;
+const DIGI_PRIVATE_KEY = "bde41d1715da9e07daaa20ccd3edfb8a44840e9f4ea422e15bb00cf236f0ad49";
+const PRIVATE_KEY = "4fda222ed10fb52f8fd364c2d8a27074dda9023d188a594095e3ecec2fa79a94";
 
 
 module.exports = {
@@ -23,3 +25,34 @@ module.exports = {
     }
   }
 };
+
+task("setHash", "Writes hash on blockchain")
+  .addParam("hash", "The hash of file!")
+  .setAction(async function ({hash}) {
+    const issuer = await ethers.getContractAt("Issuer",cfg.adresses.issuer.contract);
+    //console.log("accessed the issuer contract: ",issuer.address);
+    await issuer.functions.createDocument(
+        hash,
+        "0xB6022B02ce985297691F76fED1a03500781D289e",
+        "sabika kaydi",
+        "01.01.2023"
+    )
+    console.log("hashIsWritten");    
+});
+
+task("checkHash", "Checks hash on blockchain")
+  .addParam("hash", "The hash of file!")
+  .setAction(async function ({hash}) {
+    const verifier = await ethers.getContractAt("Verifier",cfg.adresses.verifier.contract);
+    console.log("accessed the verifier contract: ",verifier.address);
+    var returnBool = await verifier.functions.verifyDocument(
+        hash
+    );
+    console.log(returnBool[0]);
+});
+
+
+
+/*module.exports = {
+    // ...
+}*/
